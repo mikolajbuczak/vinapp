4a595d1d6bf1a4b970686c0cd01f8c7744d6a09e
Mikołaj Buczak <spam.mikolaj.buczak@gmail.com>
2021-01-23 17:44:35 +0100

    Created basic layout and added a logo file

--- src/main.wx.lua
+++ src/main.wx.lua
@@ -20,21 +20,49 @@ ID_PLAY_SELECTED_TRACK_BUTTON   = 16
 
 ID__MAX                         = 17
 
-frame = wx.wxFrame(wx.NULL, wx.wxID_ANY, "VinApp", wx.wxDefaultPosition, wx.wxSize(400, 500), wx.wxDEFAULT_FRAME_STYLE)
+--wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX)
+dialog = wx.wxDialog(wx.NULL, wx.wxID_ANY, "VinApp", wx.wxDefaultPosition, wx.wxSize(400, 500))
+panel = wx.wxPanel(dialog, wx.wxID_ANY)
+local time = wx.wxStaticText(panel, ID_TIME_LABEL, "00:00", wx.wxPoint(10, 10), wx.wxSize(30, 30))
+local title = wx.wxStaticText(panel, ID_TIME_LABEL, "Title", wx.wxPoint(50, 10), wx.wxSize(340, 30))
+local duration = wx.wxSlider(panel, ID_DURATION_BAR, 0, 0, 100, wx.wxPoint(0, 35), wx.wxSize(390, 30))
 
-frame:Connect(wx.wxID_ANY, wx.wxEVT_COMMAND_BUTTON_CLICKED,
+local topLine = wx.wxStaticLine(panel, wx.wxID_ANY, wx.wxPoint(0, 69), wx.wxSize(400, 1))
+
+local randomButton = wx.wxButton(panel, ID_RANDOM_BUTTON, "Random", wx.wxPoint(210, 80), wx.wxSize(55, 30))
+local repeatButton = wx.wxButton(panel, ID_REPEAT_BUTTON, "Repeat", wx.wxPoint(270, 80), wx.wxSize(55, 30))
+local selectFileButton = wx.wxButton(panel, ID_SELECT_FILE_BUTTON, "Select", wx.wxPoint(330, 80), wx.wxSize(55, 30))
+
+local backwardsButton = wx.wxButton(panel, ID_BACKWARDS_BUTTON, "Back", wx.wxPoint(10, 125), wx.wxSize(40, 30))
+local playButton = wx.wxButton(panel, ID_PLAY_BUTTON, "Play", wx.wxPoint(55, 125), wx.wxSize(40, 30))
+local pauseButton = wx.wxButton(panel, ID_PAUSE_BUTTON, "Pause", wx.wxPoint(100, 125), wx.wxSize(40, 30))
+local stopButton = wx.wxButton(panel, ID_STOP_BUTTON, "Stop", wx.wxPoint(145, 125), wx.wxSize(40, 30))
+local forwardButton = wx.wxButton(panel, ID_FORWARD_BUTTON, "Next", wx.wxPoint(190, 125), wx.wxSize(40, 30))
+
+local volumeBar = wx.wxSlider(panel, ID_VOLUME_BAR, 0, 0, 100, wx.wxPoint(250, 130), wx.wxSize(105, 30))
+local muteButton = wx.wxButton(panel, ID_VOLUME_BUTTON, "M", wx.wxPoint(355, 125), wx.wxSize(30, 30))
+
+local midLine = wx.wxStaticLine(panel, wx.wxID_ANY, wx.wxPoint(0, 165), wx.wxSize(400, 1))
+
+local bottomLine = wx.wxStaticLine(panel, wx.wxID_ANY, wx.wxPoint(0, 419), wx.wxSize(400, 1))
+
+local addToPlaylistButton = wx.wxButton(panel, ID_ADD_TO_PLAYLIST_BUTTON, "ADD", wx.wxPoint(10, 430), wx.wxSize(40, 30))
+local removeFromPlaylistButton = wx.wxButton(panel, ID_REMOVE_FROM_PLAYLIST_BUTTON, "REM", wx.wxPoint(55, 430), wx.wxSize(40, 30))
+local playSelectedButton = wx.wxButton(panel, ID_PLAY_SELECTED_TRACK_BUTTON, "SELECT", wx.wxPoint(100, 430), wx.wxSize(60, 30))
+
+dialog:Connect(wx.wxID_ANY, wx.wxEVT_COMMAND_BUTTON_CLICKED,
   function(event)
     -- button logic
   end
 )
 
-frame:Connect(wx.wxEVT_CLOSE_WINDOW,
+dialog:Connect(wx.wxEVT_CLOSE_WINDOW,
     function (event)
-        frame:Destroy()
+        dialog:Destroy()
         event:Skip()
     end)
 
-frame:Centre()
-frame:Show(true)
+dialog:Centre()
+dialog:Show(true)
 
 wx.wxGetApp():MainLoop()
